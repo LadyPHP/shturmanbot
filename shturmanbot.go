a31ba655c223789084df6e55a85755e0a6a85688
Alex <akoloskova@synergy.ru>
2019-02-09 16:16:48 +0300

    debug: test getMe request

--- shturmanbot.go
+++ shturmanbot.go
@@ -29,24 +29,12 @@ func main() {
 		log.Panic(err)
 	}
 
-	bot.Debug = false
-
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	//updates, err := bot.GetUpdatesChan(u)
-	updates := bot.ListenForWebhook("/" + bot.Token)
+	bot.Debug = true
 
 	if err != nil {
 		log.Panic(err)
 	}
-	// В канал updates будут приходить все новые сообщения.
-	for update := range updates {
-		// Создав структуру - можно её отправить обратно боту
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
-		bot.Send(msg)
-	}
+
+	status_message, err := bot.GetMe()
+	fmt.Println(status_message)
 }
